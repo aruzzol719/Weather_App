6e076714ac6aca6318e1bea3370bfdbbcfc303c4
MD. UZZOL HOSSAIN <aruzzol719csejnu@gmail.com>
2026-08-05 09:08:35 +0600

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { getWeather } from "./services/weatherApi.jsx";
+import { getWeather } from "./services/weatherApi";
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 import WeatherStats from "./components/WeatherStats";
